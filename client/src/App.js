f4bb767a877d2136ab3f550edcc4434179005c70
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-30 18:42:09 +0200

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
+
+import OtherPage  from './OtherPage';
+import Fib from './Fib'; 
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h2 >Fibonacci Calculator - Sample App with multi Docker instances in K8S World</h2>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div> 
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
